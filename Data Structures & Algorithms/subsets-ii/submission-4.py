abf06d86a283abb2d6831b14d2ddb309edf47f28
Lennart Schaeffer <154393333+lennartschaeffer@users.noreply.github.com>
2026-04-25 11:51:24 -0300

    Add: subsets-ii - submission-4

---
+++ Data Structures & Algorithms/subsets-ii/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        """
+            similar to combination sum II in the sense that we can have duplicates
+            but have to check that we're not going into a duplicate branch from 
+            the same lvl. 
+        """
+
+        res = []
+        curr = []
+        nums.sort()
+
+        def backtrack(i):
+            res.append(curr.copy())
+
+            for j in range(i,len(nums)):
+                if j > i and nums[j] == nums[j-1]:
+                    continue
+                curr.append(nums[j])
+                backtrack(j+1)
+                curr.pop()
+        
+        backtrack(0)
+
+        return res
+
